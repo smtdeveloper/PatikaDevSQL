7de87a8c3f0c7c531ba863fc8d32ac67a2d9e996
Samet Akca <developer2@cevizsoft.com>
2022-09-28 19:27:41 +0300

    Odev4

---
+++ Odev4.sql
@@ -0,0 +1,17 @@
+-- 1-) film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+SELECT DISTINCT replacement_cost FROM film;
+
+-- 2-) ilm tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+-- 3-) film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+SELECT * FROM film
+WHERE title ILIKE'T%' AND rating = 'G';
+
+-- 4-) country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+SELECT * FROM country
+WHERE country LIKE'_____';
+
+-- 5-) city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+SELECT * FROM city
+WHERE city ILIKE'%R';
