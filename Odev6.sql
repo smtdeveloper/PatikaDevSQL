5982951d94d7fad00348f9e93f2682057a95c540
Samet Akca <developer2@cevizsoft.com>
2022-09-29 11:14:37 +0300

    Odev6

---
+++ Odev6.sql
@@ -0,0 +1,18 @@
+-- 1) film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+SELECT ROUND (AVG(rental_rate),3) FROM film ;
+
+-- 2) film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+SELECT COUNT(title) FROM film
+WHERE title  LIKE 'C%';
+
+-- 3) film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+SELECT MAX(length) FROM film
+WHERE rental_rate=0.99;
+
+-- 4) film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+SELECT DISTINCT(replacement_cost) FROM film
+WHERE length>150; 
+
+-- ADET SAYISI !
+SELECT COUNT(DISTINCT(replacement_cost)) FROM film
+WHERE length>150; 
