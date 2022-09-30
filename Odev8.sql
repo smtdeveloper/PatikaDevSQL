4fd4381d851850384362b618916944a326cfcd8c
Samet Akca <developer2@cevizsoft.com>
2022-09-30 23:49:37 +0300

    Odev8

---
+++ Odev8.sql
@@ -0,0 +1,108 @@
+-- 1) test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+
+--CREATE TABLE employee (
+ -- id SERIAL PRIMARY KEY,
+ -- name VARCHAR(50) NOT NULL,
+--  birthday DATE
+ -- email VARCHAR(50)
+--);
+
+-- 2) Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+insert into emloyee (name, birthday, email) values ('Nerissa Pawellek', '2015-08-25', 'npawellek0@sourceforge.net');
+insert into emloyee (name, birthday, email) values ('Phil Chesson', '2003-01-31', 'pchesson1@dmoz.org');
+insert into emloyee (name, birthday, email) values ('Leontine Holyard', null, 'lholyard2@hatena.ne.jp');
+insert into emloyee (name, birthday, email) values ('Mano Hamby', '2001-10-07', 'mhamby3@wikispaces.com');
+insert into emloyee (name, birthday, email) values ('Rudie Orto', '2004-01-05', 'rorto4@cbsnews.com');
+insert into emloyee (name, birthday, email) values ('Newton Harrad', null, 'nharrad5@homestead.com');
+insert into emloyee (name, birthday, email) values ('Enid Synke', '2009-05-09', 'esynke6@ow.ly');
+insert into emloyee (name, birthday, email) values ('Damiano Huws', null, 'dhuws7@behance.net');
+insert into emloyee (name, birthday, email) values ('Morlee Mansford', '2015-06-18', 'mmansford8@uiuc.edu');
+insert into emloyee (name, birthday, email) values ('Noah Goodhand', null, 'ngoodhand9@gnu.org');
+insert into emloyee (name, birthday, email) values ('Carey Cornewall', null, 'ccornewalla@freewebs.com');
+insert into emloyee (name, birthday, email) values ('Abel Rainforth', '2021-12-21', null);
+insert into emloyee (name, birthday, email) values ('Orton Thurber', '2018-12-28', 'othurberc@yellowpages.com');
+insert into emloyee (name, birthday, email) values ('Tamar Cristou', '2006-02-10', 'tcristoud@flickr.com');
+insert into emloyee (name, birthday, email) values ('Nonah Wignall', null, 'nwignalle@cyberchimps.com');
+insert into emloyee (name, birthday, email) values ('Nert Denisevich', null, 'ndenisevichf@ifeng.com');
+insert into emloyee (name, birthday, email) values ('Leonanie West', '2013-09-03', 'lwestg@google.cn');
+insert into emloyee (name, birthday, email) values ('Dolph Craufurd', '2017-02-20', 'dcraufurdh@ihg.com');
+insert into emloyee (name, birthday, email) values ('Madeleine Alison', '2012-06-08', 'malisoni@ihg.com');
+insert into emloyee (name, birthday, email) values ('Timothea Petroff', '2018-01-21', 'tpetroffj@forbes.com');
+insert into emloyee (name, birthday, email) values ('Lay Scarrott', '2009-12-27', null);
+insert into emloyee (name, birthday, email) values ('Sonia Rowaszkiewicz', '2014-11-19', 'srowaszkiewiczl@scribd.com');
+insert into emloyee (name, birthday, email) values ('Beverie Francecione', '2018-08-23', 'bfrancecionem@cisco.com');
+insert into emloyee (name, birthday, email) values ('Benedetta Pelchat', '2003-11-05', null);
+insert into emloyee (name, birthday, email) values ('Georgi Hayhoe', '2011-01-23', 'ghayhoeo@technorati.com');
+insert into emloyee (name, birthday, email) values ('Martino Schenfisch', '2016-01-01', 'mschenfischp@creativecommons.org');
+insert into emloyee (name, birthday, email) values ('Tonia Jollands', '2002-06-01', 'tjollandsq@washington.edu');
+insert into emloyee (name, birthday, email) values ('Orelia Phoebe', '2016-10-16', 'ophoeber@phoca.cz');
+insert into emloyee (name, birthday, email) values ('Mose McGowan', null, 'mmcgowans@microsoft.com');
+insert into emloyee (name, birthday, email) values ('Emelia Wicken', '2012-10-03', 'ewickent@google.it');
+insert into emloyee (name, birthday, email) values ('Dory Ledger', '2022-03-29', null);
+insert into emloyee (name, birthday, email) values ('Gaby Meert', '2004-12-20', 'gmeertv@umich.edu');
+insert into emloyee (name, birthday, email) values ('Timoteo Ianetti', '2004-01-13', 'tianettiw@japanpost.jp');
+insert into emloyee (name, birthday, email) values ('Marline Foxley', '2015-09-30', 'mfoxleyx@mapy.cz');
+insert into emloyee (name, birthday, email) values ('Bayard Odlin', '2014-04-08', 'bodliny@ox.ac.uk');
+insert into emloyee (name, birthday, email) values ('Shandeigh Loos', '2011-10-04', 'sloosz@admin.ch');
+insert into emloyee (name, birthday, email) values ('Jarad Fredi', '2015-08-22', 'jfredi10@sciencedirect.com');
+insert into emloyee (name, birthday, email) values ('Pauly Cicullo', '2001-04-25', 'pcicullo11@fema.gov');
+insert into emloyee (name, birthday, email) values ('Kira Pragnell', null, 'kpragnell12@friendfeed.com');
+insert into emloyee (name, birthday, email) values ('Holly-anne Lambard', '2013-04-13', 'hlambard13@histats.com');
+insert into emloyee (name, birthday, email) values ('Aline Poluzzi', '2015-05-11', 'apoluzzi14@google.com');
+insert into emloyee (name, birthday, email) values ('Joan Gowrie', '2009-12-29', 'jgowrie15@webnode.com');
+insert into emloyee (name, birthday, email) values ('Larissa Bidgod', '2011-11-29', 'lbidgod16@163.com');
+insert into emloyee (name, birthday, email) values ('Angelico Jimmes', '2018-03-08', 'ajimmes17@youtube.com');
+insert into emloyee (name, birthday, email) values ('Miguel Ingrem', '2009-03-02', 'mingrem18@bluehost.com');
+insert into emloyee (name, birthday, email) values ('Agata Gulliford', '2012-05-21', 'agulliford19@dailymail.co.uk');
+insert into emloyee (name, birthday, email) values ('Wittie Oddey', '2012-09-20', 'woddey1a@cyberchimps.com');
+insert into emloyee (name, birthday, email) values ('Rurik Loades', '2007-05-31', 'rloades1b@blog.com');
+insert into emloyee (name, birthday, email) values ('Dannie O''Lyhane', '2007-06-25', 'dolyhane1c@tmall.com');
+insert into emloyee (name, birthday, email) values ('Fabian Keogh', '2006-09-12', 'fkeogh1d@mysql.com');
+
+-- 3) Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+
+UPDATE emloyee
+SET 
+name = 'Samet AKCA',
+birthday = '1999-06-07',
+email = 'smtakca99@gmail.com'
+WHERE id = 1;
+
+UPDATE emloyee
+SET 
+name = 'Eslem Betul AKCA',
+birthday = '2022-06-07',
+email = 'eslem@gmail.com'
+WHERE id = 2;
+
+UPDATE emloyee
+SET 
+name = 'Burak DEMİRKIRAN',
+birthday = '1998-11-11',
+email = 'BURKİ@gmail.com'
+WHERE id = 3;
+
+UPDATE emloyee
+SET 
+name = 'SMTcoder',
+birthday = '2020-01-01',
+email = 'smtcoder@gmail.com'
+WHERE id = 4;
+
+UPDATE emloyee
+SET 
+name = 'xxxxxxx',
+birthday = '2022-01-01',
+email = 'xxxx@gmail.com'
+WHERE id = 5;
+
+
+-- 4) Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+
+DELETE FROM emloyee
+WHERE id 	IN(6,7,8,9,10)
+RETURNING *;
+
+
+
+
