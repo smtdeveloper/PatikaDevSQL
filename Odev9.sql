9b599dfdd1c9bf03bada025b3f3dec806ae58db1
Samet Akca <developer2@cevizsoft.com>
2022-10-02 23:31:38 +0300

    Odev9-UPDATE

--- Odev9.sql
+++ Odev9.sql
@@ -1,17 +1,13 @@
 -- 1-) city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
-SELECT * FROM city;
 
-SELECT * FROM country
-Where country ILIKE'A%'
-ORDER BY country;
-
-Select * FROM film
-Where replacement_cost > 10
-ORDER BY title;
+SELECT city, country FROM city 
+JOIN country ON city.country_id  = country.country_id;
 
 
+-- 2) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+JOIN payment ON payment.customer_id = customer.customer_id;
 
--- 2-) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
-
-SELECT * FROM customer;
-
+-- 3) customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
+JOIN rental ON rental.customer_id = customer.customer_id;
